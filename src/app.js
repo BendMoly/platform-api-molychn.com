e0944e8a0a71dd96b88b698f12ba6a8a59086c52
BendMoly <ssomkhyy@qq.com>
2018-08-15 23:23:13 +0800

    修改入口文件路由

--- src/app.js
+++ src/app.js
@@ -5,34 +5,34 @@ const bodyParser = require('koa-bodyparser')
 const auth = require('koa-basic-auth')
 const mount = require('koa-mount')
 
-const Router = require('koa-router')
+// const Router = require('koa-router')
+const user = require('./apis/user')
 // 基础配置
 const configuration = require('./config')
 
 const app = new Koa()
 app.use(bodyParser())
 app.use(cors())
-const router = new Router()
-router.get('/admin/:id', async(ctx, next) => {
-  console.log('admin')
-  ctx.response.status = 200
-  ctx.response.body = 'is admin'
-  await next()
-})
-router.get('/normal', async(ctx, next) => {
-  console.log('normal')
-  ctx.response.status = 200
-  ctx.response.body = 'is normal'
-  await next()
-})
+// const router = new Router()
+// router.get('/admin/:id', async(ctx, next) => {
+//   console.log('admin')
+//   ctx.response.status = 200
+//   ctx.response.body = 'is admin'
+//   await next()
+// })
+// router.get('/normal', async(ctx, next) => {
+//   console.log('normal')
+//   ctx.response.status = 200
+//   ctx.response.body = 'is normal'
+//   await next()
+// })
 
-// app.use(auth({ name: 'tj', pass: 'tobi' }))
 app.use(mount('/admin', auth({ name: 'molychn', pass: 'lincanyue' })))
 app.use(async (ctx, next) => {
-  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-  await next();
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
+  await next()
 })
-app.use(router.routes())
+app.use(user.routes())
 
 app.listen(configuration.PORT, '0.0.0.0', () => {
   console.log(`app start at port ${configuration.PORT}`)
